7e8afd57d601ea2837500299346c20bc9e265ea1
Øyvind Granberg <granberg@tresfjording.no>
2025-12-25 01:21:23 +0100

    pynting

--- script.js
+++ script.js
@@ -73,7 +73,8 @@ function oppdaterInfo(entry) {
   document.getElementById('tilskuddDisplay').textContent = entry.tilskudd ?? 'Ukjent';
   document.getElementById('språkDisplay').textContent = entry.språk ?? 'Ukjent';
   document.getElementById('k_slagordDisplay').textContent = entry.k_slagord || 'Ingen slagord registrert';
-  document.getElementById('f_slagordDisplay').textContent = entry.f_slagord || 'Ingen slagord registrert';
+  document.getElementById('f_slagordDisplay').textContent = entry.f_slagord || 'Ingen slagord regitrert';
+  document.getElementById("valgtKommuneDisplay").textContent = entry.kommune;
 }
 
 // -----------------------------
