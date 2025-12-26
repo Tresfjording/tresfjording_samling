ab5fa3a7e8ea1a5bd2081a4dfacb5f2b43039c5c
Øyvind Granberg <granberg@tresfjording.no>
2025-12-26 22:10:14 +0100

    test

--- script.js
+++ script.js
@@ -128,7 +128,7 @@ function visPåKart(map, entry) {
     .openPopup();
 
   // Zoom inn
-  map.setView([entry.lat_decimal, entry.lon_decimal], 12);
+  map.setView([entry.lat_decimal, entry.lon_decimal], 120);
 }
 
 
